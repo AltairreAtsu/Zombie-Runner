863809c5a58f4021d3688a1c3be02966d4459863
Marc Taurasi <altairre.atsu@gmail.com>
2018-01-01 00:39:32 -0700

    Made Gun Object move with Camera and fine tuned animations

--- Assets/Scripts/FireGun.cs
+++ Assets/Scripts/FireGun.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 
 public class FireGun : MonoBehaviour {
 
+	[SerializeField][Tooltip ("The Layer ID to use for collisions")]
+	private int layerNumber = 8;
+	[SerializeField][Tooltip ("The maximum distance a Raycast from the gun can travel.")]
+	private float maxDistance = 100f;
+
 	private void Fire (){
 		Vector3 fwd = transform.TransformDirection (Vector3.forward);
 		RaycastHit hit;
 		int layerMask = 1 << 8;
 
-		if(Physics.Raycast(transform.position, fwd, out hit, 20f, layerMask)){
+		if(Physics.Raycast(transform.position, fwd, out hit, maxDistance, layerMask)){
 			ZombieLogic logic = hit.collider.gameObject.GetComponent<ZombieLogic> ();
 			if(logic != null){
 				logic.Hit (1);
