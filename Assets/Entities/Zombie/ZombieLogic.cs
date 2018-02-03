2ed4507d94fc5fd344b6be182f0d2f358a05d7cb
Marc Taurasi <altairre.atsu@gmail.com>
2018-02-03 11:25:38 -0700

    Re-added Zombie Attack Tell Sound Effects and delay.

--- Assets/Entities/Zombie/ZombieLogic.cs
+++ Assets/Entities/Zombie/ZombieLogic.cs
@@ -4,36 +4,78 @@ using UnityEngine;
 using UnityStandardAssets.Characters.ThirdPerson;
 
 public class ZombieLogic : MonoBehaviour {
-	[SerializeField][Tooltip ("Range from which Zombies can hit the player.")]
+	[SerializeField]
+    [Tooltip ("Range from which Zombies can hit the player.")]
 	private float attackDistance = 2.5f;
-	[SerializeField][Tooltip ("Number of hits a zombie can take before dying.")]
+
+    [SerializeField]
+    [Tooltip ("Delay from playing the attack sound to damaging the player.")]
+    private float attackDelay = 1f;
+
+    [SerializeField]
+    [Tooltip("Damage done by zombie attacks.")]
+    private float damage = 1f;
+
+    [Space]
+
+	[SerializeField]
+    [Tooltip ("Number of hits a zombie can take before dying.")]
 	private float health = 2;
-	[SerializeField][Tooltip ("Time after a hit before another hit can be registered.")]
+
+	[SerializeField]
+    [Tooltip ("Time after a hit before another hit can be registered.")]
 	private float invicabilityTime = 0.3f;
-	[SerializeField][Tooltip ("Sound Clip to be played when the zombie dies.")]
-	private AudioClip deathClip = null;
+
+    [Space]
+
+    [SerializeField]
+    [Tooltip ("Sound Effects to be played when the zombie starts attacking.")]
+    private AudioClip[] attackSounds = null;
 
 	private AudioSource audioSource = null;
-	private Player Player;
+	private Player Player = null;
+    private bool attacking = false;
 	private float timeSinceHit = 0f;
 
 	public delegate void OnZombieKilled(Transform zombiePostion);
 	public static OnZombieKilled OnZombieKilledObservers = null;
 
-	void Start () {
+	private void Start () {
 		Player = GameObject.FindObjectOfType<Player> ();
 		audioSource = GetComponent<AudioSource> ();
 
 		gameObject.GetComponent<AICharacterControl> ().target = Player.transform;
 	}
 
-	void Update () {
+	private void Update ()
+    {
 		timeSinceHit += Time.deltaTime;
-		if(DistanceToPlayer() <= attackDistance){
-			// TODO Play Attack Animation
-			Player.Hit (1);
+		if(DistanceToPlayer() <= attackDistance && !attacking)
+        {
+            // TODO Play Attack Animation
+            attacking = true;
+            audioSource.clip = attackSounds[Random.Range(0, attackSounds.Length)];
+            audioSource.Play();
+
+            float delay = attackDelay;
+
+            if(audioSource.clip.length > attackDelay)
+            {
+                delay = attackDelay + (audioSource.clip.length - attackDelay);
+            }
+
+            Invoke("Attack", delay);
 		}
 	}
+
+    private void Attack()
+    {
+        attacking = false;
+        if (DistanceToPlayer() <= attackDistance)
+        {
+            Player.Hit(damage);
+        }
+    }
 
 	public void Hit (float damage) {
 		if (timeSinceHit >= invicabilityTime) {
